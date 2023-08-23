15f479d24e100153e48efae33a084b5e6adf3c85
Sunnyiitg <sunnyfromiitg@gmail.com>
2023-08-24 00:06:45 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL,{
+      useNewUrlParser:true,
+      useUnifiedTopology:true
+    });
+    console.log(
+      `Connected to Mongodb Database ${mongoose.connection.host}`.bgMagenta
+        .white
+    );
+  } catch (error) {
+    console.log(`MONGO Connect Error ${error}`.bgRed.white);
+  }
+};
+
+module.exports = connectDB;
